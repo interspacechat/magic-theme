7398491a43a7bfc3c3ef870c8b170145bb44f474
jascha <jascha@jaeh.at>
2020-03-16 06:46:56 +0100

    Logo src is not relative to page root

--- src/Logo.mjs
+++ src/Logo.mjs
@@ -1,6 +1,6 @@
 export const View = state =>
   Link({ to: state.root, class: 'Logo' }, [
-    Img('/img/interspacechat.png'),
+    Img({ src: '/img/interspacechat.png' }),
   ])
 
 export const style = {
